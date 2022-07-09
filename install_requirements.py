526ccf72cbc21e0be217d559db4a9ba406febec4
Shiraz <smcclennon@protonmail.com>
2022-07-09 10:07:34 +0100

    Show error message on requirements install failure

--- install_requirements.py
+++ install_requirements.py
@@ -8,6 +8,6 @@ try:
     print("Installing Python packages and dependencies from requirements.txt...\n")
     os.system('py -m pip install -r requirements.txt' if os.name=='nt' else 'pip install -r requirements.txt')
     print("\nStart pluGET by launching the 'pluGET.py' file!")
-except:
-    print("Requirements couldn't be installed. Check if file 'requirements.txt' is in the same folder and that Python3\
+except Exception as e:
+    print(f"Requirements couldn't be installed.\nError message: {e}\n\nCommon solution: Check if file 'requirements.txt' is in the same folder and that Python3\
          and pip is installed!")
